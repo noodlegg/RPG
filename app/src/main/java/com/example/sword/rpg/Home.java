4f2ac2d3fc3ab0687d2326ab8605f0fcf241453e
NoodleGG <TGian98@gmail.com>
2018-02-28 11:20:18 +0100

    Home buttons and SwipeMechanic updated
    Home button clicks opens the other activities for testing.
    Solo -> SwipeMechanic
    Party -> ShakeMechanic
    Online -> MultiplayerConnection
    
    SwipeMechanic now logs taps, flings etc in the Logcat. No directional swipes and UI updates yet.
    
    Dependencies have been added from beginner workshop.
    
    Broken:
    - Selecting other navigation bar items i.e. Dashboard & Notifications

--- app/src/main/java/com/example/sword/rpg/Home.java
+++ app/src/main/java/com/example/sword/rpg/Home.java
@@ -1,10 +1,13 @@
 package com.example.sword.rpg;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
@@ -39,6 +42,46 @@ public class Home extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Button playSolo = (Button) findViewById(R.id.solo_mode);
+        Button playParty = (Button) findViewById(R.id.party_mode);
+        Button playOnline = (Button) findViewById(R.id.online_mode);
+        // Solo opens SwipeMechanic activity
+        playSolo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSwipeMechanic();
+            }
+        });
+        // Party opens ShakeMechanic activity
+        playParty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startShakeMechanic();
+            }
+        });
+        // Online opens MultiplayerConnection activity
+        playOnline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMC();
+            }
+        });
+    }
+
+    public void startSwipeMechanic() {
+        Intent intent = new Intent(this, SwipeMechanic.class);
+        startActivity(intent);
+    }
+
+    public void startShakeMechanic() {
+        Intent intent = new Intent(this, ShakeMechanic.class);
+        startActivity(intent);
+    }
+
+    public void startMC() {
+        Intent intent = new Intent(this, MultiplayerConnection.class);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/example/sword/rpg/SwipeMechanic.java
+++ app/src/main/java/com/example/sword/rpg/SwipeMechanic.java
@@ -1,13 +1,93 @@
 package com.example.sword.rpg;
 
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.TextView;
 
-public class SwipeMechanic extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class SwipeMechanic extends AppCompatActivity implements
+        GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener {
+
+    private static final String DEBUG_TAG = "Gestures";
+    private GestureDetectorCompat mDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe_mechanic);
+        // Text for testing
+        TextView textView = (TextView) findViewById(R.id.textView);
+        //Initiate gesture detector
+        mDetector = new GestureDetectorCompat(this, this);
+        // Set gesture detector as double tap listener
+        mDetector.setOnDoubleTapListener(this);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        if (this.mDetector.onTouchEvent(event)) {
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent event) {
+        Log.d(DEBUG_TAG,"onDown: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2,
+                           float velocityX, float velocityY) {
+        Log.d(DEBUG_TAG, "onFling: " + event1.toString() + event2.toString());
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onLongPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent event1, MotionEvent event2, float distanceX,
+                            float distanceY) {
+        Log.d(DEBUG_TAG, "onScroll: " + event1.toString() + event2.toString());
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onShowPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onSingleTapUp: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDoubleTap: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDoubleTapEvent: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onSingleTapConfirmed: " + event.toString());
+        return true;
     }
 }
