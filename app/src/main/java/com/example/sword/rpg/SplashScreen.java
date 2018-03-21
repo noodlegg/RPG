8f67aa400e8c89668522efd91f4da0f4c488365d
NoodleGG <TGian98@gmail.com>
2018-03-21 10:24:26 +0100

    Splash screen added
    - Splash screen shows up on start
    - Drawables & font imported
    - Style in manifest changed to NoActionBar, to remove title bar
    
    TBD:
    - Selecting items from bottomNavBar should open new fragment
    - "Party" button opens the party name input activity

---
+++ app/src/main/java/com/example/sword/rpg/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.example.sword.rpg;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+/**
+ * Created by s152706 on 12-Mar-18.
+ */
+
+public class SplashScreen extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this, Home.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
+
