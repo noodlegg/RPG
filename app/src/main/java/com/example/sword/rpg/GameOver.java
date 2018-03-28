22201df0f59e5788d59d45276c71eb666927e009
NoodleGG <TGian98@gmail.com>
2018-03-28 12:06:09 +0200

    Solo Game added
    - SoloGame with corresponding files for swipe commands has been added
    - Exited activities are destroyed to prevent unintended access to activities through the on-screen back button
    
    TBD:
    - High score upload to Firebase
    - Add different commands to gameplay
    - Party mode implementation
    - Settings functionality

---
+++ app/src/main/java/com/example/sword/rpg/GameOver.java
@@ -0,0 +1,86 @@
+package com.example.sword.rpg;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class GameOver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over);
+
+        // The Home button
+        Button homeButton = findViewById(R.id.home_button);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {toHome(); }
+        });
+
+        // The Retry button
+        Button retryButton = findViewById(R.id.retry_button);
+        retryButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {playAgain(); }
+        });
+
+        // Set the correct score in the text
+        TextView scoreTextView = findViewById(R.id.score);
+        Bundle bundle = getIntent().getExtras(); // Get the Bundle (with the score)
+        Integer score = bundle.getInt("score"); // Fetch score from bundle
+        scoreTextView.setText(score.toString()); // Set score as text
+
+        // Handle high scores
+        TextView highScoreText = findViewById(R.id.text_highscore);
+        handleHighScores(highScoreText, score);
+    }
+
+    /**
+     * Handles high score checks and displays.
+     * Checks if a high score was beaten and if so, updates it.
+     * Also displays the (old) high score.
+     * @param highScoreText  TextView in layout that should display the high score
+     * @param score  score from recently finished game
+     */
+    private void handleHighScores(TextView highScoreText, Integer score) {
+        // Get shared data (preferences)
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        Integer oldHighScore = prefs.getInt("highScore", 0); // Get high score from preferences
+
+        // If the old high score was beaten:
+        if (score > oldHighScore) {
+            // Display the old high score
+            highScoreText.setText("Previous high score: " + oldHighScore);
+
+            SharedPreferences.Editor editor = prefs.edit(); // Create editor to change shared data
+            editor.putInt("highScore", score); // Update the high score
+            editor.apply(); // Apply changes
+        } else { // (if the high score was not beaten: )
+            // Display current (unbeaten) high score
+            highScoreText.setText("High score: " + oldHighScore);
+        }
+    }
+
+    /**
+     * Opens the Home activity.
+     */
+    protected void toHome() {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+        finish();
+    }
+
+    /**
+     * Starts a new solo game
+     */
+    protected void playAgain() {
+        Intent intent = new Intent(this, SoloGame.class);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/sword/rpg/SwipeCommandFragment.java
@@ -0,0 +1,132 @@
+package com.example.sword.rpg;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class SwipeCommandFragment extends Fragment {
+
+    private int direction;
+    private View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Define view
+        view = inflater.inflate(R.layout.fragment_swipe_command, container, false);
+
+        ImageView arrow = view.findViewById(R.id.arrow); // Arrow image
+        /*Matrix matrix = new Matrix(); // Matrix to define scale and rotation of image
+        arrow.setScaleType(ImageView.ScaleType.MATRIX); // Set scale type of image to MATRIX*/
+
+        Random rand = new Random();
+        direction = rand.nextInt(4); // Get a random direction ([0, 3])
+
+        // Rotate the matrix
+        /*matrix.postRotate((float) (90 * direction),
+                arrow.getDrawable().getBounds().width()/2,
+                arrow.getDrawable().getBounds().height()/2);
+        arrow.setImageMatrix(matrix); // Match scale of image to the matrix*/
+
+        arrow.setRotation(90* direction);
+        updateText(direction); // Set the correct command title
+
+
+        final GestureDetector gesture = new GestureDetector(getActivity(),
+                new GestureDetector.SimpleOnGestureListener() {
+
+                    @Override
+                    public boolean onDown(MotionEvent e) {
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+                                           float velocityY) {
+                        System.out.println("onFling() has been called");
+                        final int SWIPE_MIN_DISTANCE = 150; // To detect long enough swipes
+                        final int SWIPE_MAX_OFF_PATH = 150; // To ignore diagonal swipes
+                        final int SWIPE_THRESHOLD_VELOCITY = 200; // To ignore slow swipes
+
+                        // If the swipe wasn't "too vertical", check horizontal swipes
+                        if (Math.abs(e1.getY() - e2.getY()) < SWIPE_MAX_OFF_PATH) {
+                            // To the left
+                            if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE
+                                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                                swipeDetected(3);
+
+                                // To the right
+                            } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE
+                                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                                swipeDetected(1);
+                            }
+                        }
+
+                        //If the swipe wasn't "too horizontal", check vertical swipes
+                        if (Math.abs(e1.getX() - e2. getX()) < SWIPE_MAX_OFF_PATH) {
+                            // Upward
+                            if (e1.getY() - e2.getY() > SWIPE_MIN_DISTANCE
+                                    && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                                swipeDetected(0);
+
+                                // Downward
+                            } else if (e2.getY() - e1.getY() > SWIPE_MIN_DISTANCE
+                                    && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+                                swipeDetected(2);
+                            }
+                        }
+                        return super.onFling(e1, e2, velocityX, velocityY);
+                    }
+                });
+
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                return gesture.onTouchEvent(event);
+            }
+        });
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+    private void swipeDetected(int detected) {
+        // On a correct swipe:
+        if (detected == direction) {
+            // Command succeeded
+            ((SoloGame)getActivity()).commandFinished(true);
+        } else { // (on wrong swipe:)
+            // Command failed
+            ((SoloGame)getActivity()).commandFinished(false);
+        }
+    }
+
+    private void updateText(int direction) {
+        // The command title
+        TextView textView = view.findViewById(R.id.command_title);
+        String text = "Swipe ";
+
+        // Add the correct direction to the command title
+        switch (direction) {
+            case 0: text += "up!";
+                break;
+            case 1: text += "right!";
+                break;
+            case 2: text += "down!";
+                break;
+            case 3: text += "left!";
+                break;
+        }
+
+        // Update the command title text
+        textView.setText(text);
+    }
+}
